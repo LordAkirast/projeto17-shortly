3bfb9b009d0b3f0d48e80f260ebfe026f57b09c6
LordAkirast <darkwarish@gmail.com>
2023-08-04 03:46:08 -0300

    Rota de post criada.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express"
 import cors from "cors"
-//import { db } from "./databases/database.connection.js"
+import { db } from "./databases/database.connections.js"
 import Joi from "joi"
 import dayjs from "dayjs"
+import bcrypt from "bcrypt"
 
 const app = express()
 app.use(cors())
@@ -10,19 +11,52 @@ app.use(express.json())
 
 
 
-const createGame = Joi.object({
-    stockTotal: Joi.number().required(),
-    pricePerDay: Joi.number().required(),
+const createUser = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
 });
 
+const createdAt = dayjs().format('YYYY-MM-DD HH:mm:ss');
 
 
-app.post("/signup"), async (req,res) => {
 
-    const {name, email, password, confirmPassword} = req.body
+app.post("/signup"), async (req, res) => {
 
+    const { name, email, password, confirmPassword } = req.body
 
     
+
+    const validation = createUser.validate({ name, email, password }, { abortEarly: "False" })
+    if (validation.error) {
+        console.log("erro 1")
+        const errors = validation.error.details.map((detail) => detail.message)
+        return res.status(422).send(errors);
+    }
+
+    if (password !== confirmPassword) {
+        return res.status(400).send('A senha e a confirmaçao de senha devem ser iguais.')
+    }
+
+    //encriptação
+    const passCrypt = bcrypt.hashSync(password, 10)
+
+
+    try {
+
+        userVerify = await db.query('SELECT * FROM USERS where email = $1', [email]);
+        if (userVerify) {
+            return res.status(409).send('Email já cadastrado!')
+        } else {
+            user = await db.query('INSERT INTO USERS (name, email, password, "createdAt") values ($1, $2, $3, $4, $5);', [name, email, passCrypt, createdAt]);
+            return res.status(201).send('Usuário criado!')
+        }
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+
+
+
 }
 
 
@@ -31,5 +65,5 @@ app.post("/signup"), async (req,res) => {
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-	console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${port}`)
 })
