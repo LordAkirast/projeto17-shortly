7d38c7f291185c518fc6d9a1fe9f61b8ad120ca5
LordAkirast <darkwarish@gmail.com>
2023-08-04 03:54:17 -0300

    Dump atualizado

--- dump.sql
+++ dump.sql
@@ -28,7 +28,8 @@ CREATE TABLE public.users (
     id integer NOT NULL,
     name text NOT NULL,
     email text NOT NULL,
-    password text NOT NULL
+    password text NOT NULL,
+    createdat timestamp without time zone
 );
 
 
