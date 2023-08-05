2eb25f87d7360a34edcb0c8c3355d015eebfb662
LordAkirast <darkwarish@gmail.com>
2023-08-05 14:45:40 -0300

    Rota de signin completa. tentando saber o que fazer com o token.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { db } from "./databases/database.connections.js"
 import Joi from "joi"
 import dayjs from "dayjs"
 import bcrypt from "bcrypt"
+import { v4 as uuid } from 'uuid'
 
 const app = express()
 app.use(cors())
@@ -17,6 +18,12 @@ const createUser = Joi.object({
     password: Joi.string().required(),
 });
 
+const loginUser = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+});
+
+
 const createdAt = dayjs().format('YYYY-MM-DD HH:mm:ss');
 
 
@@ -25,7 +32,7 @@ app.post('/signup', async (req, res) => {
 
     const { name, email, password, confirmPassword } = req.body
 
-    
+
 
     const validation = createUser.validate({ name, email, password }, { abortEarly: "False" })
     if (validation.error) {
@@ -59,7 +66,48 @@ app.post('/signup', async (req, res) => {
 
 })
 
-app.post('/teste', async (req,res) => {
+app.post('/signin', async (req, res) => {
+
+    const { email, password } = req.body
+
+
+    const validation = loginUser.validate({ email, password }, { abortEarly: "False" })
+    if (validation.error) {
+        console.log("erro 1 - signin")
+        const errors = validation.error.details.map((detail) => detail.message)
+        return res.status(422).send(errors);
+    }
+
+    try {
+
+        const userVerify = await db.query('SELECT * FROM USERS where email = $1', [email]);
+        console.log(userVerify.rows)
+        if (userVerify.rows.length > 0) {
+
+            const decrypt = bcrypt.compareSync(password, userVerify.rows[0].password)
+
+            if (decrypt === true) {
+                console.log('Usuário logado!')
+                const token = uuid()
+                console.log(token)
+                return res.status(200).send(({ token: token }))
+            } else {
+                console.log('senha incorreta')
+                return res.status(400).send('Senha incorreta!')
+            }
+        } else {
+            console.log('email incorreto')
+            return res.status(400).send('email incorreto!')
+        }
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+
+
+
+})
+
+app.post('/teste', async (req, res) => {
     return res.status(200).send('Sucesso')
 })
 
